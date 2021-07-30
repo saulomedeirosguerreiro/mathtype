49b8d46e9c19531a29b9db61b9a9ea28dca4e64d
Saulo Medeiros Guerreiro <sauloguerreiro@sas.com.br>
2021-07-30 15:25:16 -0300

    feat: use rich-text-viewer component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <RichTextViewer :text="html" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import RichTextViewer from "./components/RichTextViewer.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    RichTextViewer,
+  },
+  data() {
+    return {
+      html: `<span style='font-size: 16px;'>Um número composto possui <br><br>
+      <math xmlns='http://www.w3.org/1998/Math/MathML'><mroot><mn>2</mn><mn>1</mn></mroot>
+      <mo>&nbsp;</mo><mspace linebreak='newline'></mspace><mfenced open='{' close='}'><mn>3</mn></mfenced>
+      <mo>&nbsp;</mo><mo>&nbsp;</mo><mn>4</mn><mo>∈</mo><mn>5</mn><mo>&nbsp;</mo><mn>6</mn><mo>∩</mo><mn>7</mn>
+      <mspace linebreak='newline'></mspace><msup><mn>8</mn><mn>9</mn></msup><mspace linebreak='newline'></mspace>
+      <mspace linebreak='newline'></mspace><mn>10</mn><mo>∞</mo><mi mathvariant='normal'>π</mi><mo>∅</mo>
+      <mspace linebreak='newline'></mspace></math></span>`,
+    };
   },
 };
 </script>
