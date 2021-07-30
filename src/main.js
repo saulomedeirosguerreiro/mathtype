4a9a9f2a3bd4dab47bb624bb84d558d180cf44bd
Saulo Medeiros Guerreiro <sauloguerreiro@sas.com.br>
2021-07-30 15:23:26 -0300

    feat: add vue-ref dependency

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "./plugin/vue-ref";
 
 Vue.config.productionTip = false;
 
---
+++ src/plugin/vue-ref.js
@@ -0,0 +1,3 @@
+import Vue from "vue";
+import ref from "vue-ref";
+Vue.use(ref);
